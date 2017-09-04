86c3c702752926521adf367f8abb2855b27daa2e
Florian <florian.martinez@epfl.ch>
2017-09-04 14:11:46 +0200

    Correct Matlab path to find .m files
    When starting Matlab with -nojvm, the startup folder is the one from which it was started. However, when starting with the java virtual machine, the startup folder depends on the user preferences. In order to always work in the same folder, a 'cd folder' is done in Matlab before launching the script, folder being the path to the current working directory returned by 'pwd' in the bash file.

--- ius_results.sh
+++ ius_results.sh
@@ -3,12 +3,9 @@
 # Create a results folder in case it does not exist
 if [ ! -d "results" ]; then
   # Control will enter here if $DIRECTORY doesn't exist.
-  mkdir results
-  cd "results"
-  mkdir "das"
-  mkdir "FISTA"
-  mkdir "FISTALP"
-  cd ..
+  mkdir -p results/das
+  mkdir -p results/FISTA
+  mkdir -p results/FISTALP
 fi
 
 echo "*****************************   Downloading the data   *****************************"
@@ -17,10 +14,10 @@ python setup_ius.py
 echo "*****************************   Launching the image reconstruction scripts   *****************************"
 python launch_ius_scripts.py $1
 
-
 echo "*****************************   Launching the scripts generating the metrics   *****************************"
 cd matlab
-matlab -nodesktop -nojvm -r "generate_metrics({'../results/das', '../results/FISTA', '../results/FISTALP'}); exit;"
+CURR_PATH=$(pwd)
+matlab -nodesktop -nojvm -nosplash -r "cd '$CURR_PATH'; generate_metrics({'../results/das', '../results/FISTA', '../results/FISTALP'}); exit;"
 
 echo "*****************************   Launching the scripts generating the figures   *****************************"
-matlab -nodesktop -r "generate_figures({'../results/das', '../results/FISTA', '../results/FISTALP'});exit;"
+matlab -nodesktop -nosplash -r "cd '$CURR_PATH'; generate_figures({'../results/das', '../results/FISTA', '../results/FISTALP'}); exit;"
